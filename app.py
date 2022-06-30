2f6f53553e6eb5d0e2deba2618dd2c50a39c3a01
ManNy897 <mannya897@gmail.com>
2022-06-29 21:51:19 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from flask import jsonify
 from find_nearest import get_closest_hawker_locations
 
 
 app = Flask(__name__)
 cors = CORS(app)
 
-
-@app.route("/test.py" , methods=['GET'])  # consider to use more elegant URL in your JS
+@app.route("/get_hawker_locations.py" , methods=['GET'])
 def get_hawker_locations():
-    print("in get method")
-    args = request.args
-    print(args)
     latitude = request.args.get("latitude")
     longitude = request.args.get("longitude")
     result = get_closest_hawker_locations(latitude, longitude)
 
-    print(result)
     return jsonify(result)
+
+@app.route('/')
+def root():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     # here is starting of the development HTTP server
--- microsoft_prepare_data.py
+++ microsoft_prepare_data.py
@@ -2,50 +2,56 @@ import json
 import csv
 import re
 
-# import xml.etree.ElementTree as ET
-# from lxml import etree
-
-
 name_regex = '<th>NAME</th> <td>(.*?)</td>'
 photo_regex = '<th>PHOTOURL</th> <td>(.*?)</td>'
 
+#Extract the name and photo url using regex
+def get_name_and_photo_url(xml_string):
+	name_regex_result = re.search(name_regex, xml)
+	name = None
+	if name_regex_result != None:
+		name = name_regex_result.group(1)
+	else:
+		print("no name")
+
+
+	photo_regex_result = re.search(photo_regex, xml)
+	photo_url = None
+	if photo_regex_result != None:
+		photo_url = photo_regex_result.group(1)
+	else:
+		print("no photo")
+	return name, photo_url
+
+
+
+
+
+
+#read in the dataset
 with open("hawker-centres-geojson.geojson", "r") as inputfile:
 	data = json.load(inputfile)
 
+
+
+#extract required data for each line in dataset
 hawker_centres = {}
 hawker_centers_list = []
 
 for feature in data['features']:
-	# try:
-		longitude = feature["geometry"]["coordinates"][0]
-		latitude = feature["geometry"]["coordinates"][1]
-		xml = feature["properties"]["Description"]
+	longitude = feature["geometry"]["coordinates"][0]
+	latitude = feature["geometry"]["coordinates"][1]
+	xml = feature["properties"]["Description"]
 
-		name_regex_result = re.search(name_regex, xml, re.IGNORECASE)
-		name = None
-		if name_regex_result != None:
-			name = name_regex_result.group(1)
-		else:
-			print("no name")
+	name, photo_url = get_name_and_photo_url(xml)
+
+	hawker_centres[name] = {"photo_url":photo_url, "latitude":latitude, "longitude":longitude}
+	hawker_centers_list.append([name, photo_url, longitude, latitude])
 
 
-		photo_regex_result = re.search(photo_regex, xml, re.IGNORECASE)
-		photo_url = None
-		if photo_regex_result != None:
-			photo_url = photo_regex_result.group(1)
-		else:
-			print("no photo")
-
-		# print(xml)
-		# root = etree.fromstring(xml)
-		# name = root.xpath('.//table/tr/th[text()="NAME"]/../td')[0].text
-		# photo_url = root.xpath('.//table/tr/th[text()="PHOTOURL"]/../td')[0].text
-		hawker_centres[name] = {"photo_url":photo_url, "latitude":latitude, "longitude":longitude}
-		hawker_centers_list.append([name, photo_url, longitude, latitude])
-print(len(hawker_centres))
-
+#write out result
 json_dataset = json.dumps(hawker_centres, indent = 4)
-with open("json_dataset.json", "w") as outfile:
+with open("json_dataset2.json", "w") as outfile:
 	outfile.write(json_dataset)
 
 
